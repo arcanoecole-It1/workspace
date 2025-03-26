17a625835ba64c7caff7a57ee0f00d432d6d16cf
Nexus D-Prime <djiack7@gmail.com>
2025-03-26 21:50:39 +0100

    new commit

--- Html/calculatrice/cal.js
+++ Html/calculatrice/cal.js
@@ -1,19 +1,33 @@
-let buttons = document.querySelectorAll(".button")
+const buttons = document.querySelectorAll(".button");
+const symbols = document.querySelectorAll(".special");
 let calcul = document.querySelector(".ecran");
-buttons.forEach((button) => {
-    button.addEventListener("click" , () => {
-        if(calcul.textContent === '0') {
-            calcul.textContent = button.textContent
-        } else {
-            calcul.textContent += button.textContent 
-            let valeur = calcul.textContent
-        }
+buttons.forEach( btn => {
+    btn.addEventListener("click" , () => {
+        calcul.textContent += btn.textContent
     })
 })
 
-let operation = document.querySelector('.add');
-add.addEventListener('click', function(){
-    let result = 0;
-    parseInt(calcul.textContent) 
+symbols.forEach(symbol => {
+    symbol.addEventListener("click", valueInput)
+    switch (symbol.textContent) {
+        case "+":
+            addition();
+            break;
+    }
 })
 
+function valueInput() {
+    symbols.forEach(sym => {
+        let value = calcul.textContent;
+        calcul.textContent = sym.textContent;
+        return parseInt(value);
+    })
+
+}
+
+function addition() {
+    let total = 0;
+    let values = valueInput();
+    total += values;
+    return console.log(total);
+}
--- Python/chapitre 9/9.2.py
+++
@@ -1,6 +0,0 @@
-File_path = "chapitre 9/Resources/9.2.i_can_write.txt"
-try:
-    with open(File_path,mode='w',encoding='utf-8') as f:
-        f.write("I can Write")
-except Exception as e:
-    print(e)
